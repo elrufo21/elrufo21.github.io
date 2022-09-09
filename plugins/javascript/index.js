60d519ccc0cbfddbcf032f6821cff56356256220
elrufo21 <neburtnt@gmail.com>
2022-09-09 00:38:31 -0500

    v2

---
+++ plugins/javascript/index.js
@@ -0,0 +1,25 @@
+var sound = new Audio();
+sound.src = "plugins/audio/audio.mp3"
+
+
+const typed = new Typed(".typed", {
+    strings: ["", "...", "<p id='m1'>Hola mundo</p>", "<p id='m2'>Soy Ruben Dario - ElRufo</p>", "<p id='m3'>BackEnd Developer</p>", "<p id='m4'>FrontEnd Developer</p>"],
+    cursorChar: "",
+    typeSpeed: 70,
+    backSpeed: 75,
+    loop: true,
+    loopCount: false,
+});
+
+window.addEventListener('scroll', function () {
+    let animated = this.document.getElementById("animated");
+    let positionObj1 = animated.getBoundingClientRect().top;
+    console.log(positionObj1);
+
+    sound.play();
+    sound.volume = 0.08;
+
+    if (positionObj1 < 844 && positionObj1 > 380) {
+        animated.style.animation = "mover 1s ease-out"
+    }
+})
